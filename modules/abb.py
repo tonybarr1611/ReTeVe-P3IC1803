4f61c690542ec5863eb6f7cac1ca99acc7bba0dd
tony1611 <antbarrantes@estudiantec.cr>
2023-06-17 21:04:34 -0600

    Clase básica del ABB
    Creación de la clase básica del ABB y sus nodos. Actualmente solo tienen funciones de "C", quedan pendientes "RUD"

---
+++ modules/abb.py
@@ -0,0 +1,46 @@
+class Nodo:
+    def __init__(self, cita, tipo, placa, vehiculo, marca, modelo, propietario, telefono, correo, direccion, fecha, izquierda = None, derecha = None):
+        self.cita = cita
+        self.datos = {
+            'tipo': tipo,
+            'placa': placa,
+            'vehiculo': vehiculo,
+            'marca': marca,
+            'modelo': modelo,
+            'propietario': propietario,
+            'telefono': telefono,
+            'correo': correo,
+            'direccion': direccion,
+            'fecha': fecha
+        }
+        self.izquierda = izquierda
+        self.derecha = derecha
+# Se crea la clase Arbol que nos permitira crear el arbol binario
+class Arbol:
+    # Se inicializa como un arbol de 0 nodos
+    def __init__(self):
+        self.raiz = None
+        self.cantidad_citas = 0
+    # Funcion auxiliar recursiva usada para agegar un nodo al arbol
+    def agregar_nodo_aux(self, nodo_insertado, nodo_actual):
+        # Si la fecha es menor, se mueve a la izquiera
+        if nodo_insertado.datos['fecha'] < nodo_actual.datos['fecha']:
+            if nodo_actual.izquierda != None:
+                self.agregar_nodo_aux(nodo_insertado, nodo_actual.izquierda)
+            else:
+                nodo_actual.izquierda = nodo_insertado
+        # Si la fecha es mayor, se mueve a la derecha
+        else:
+            if nodo_actual.derecha != None:
+                self.agregar_nodo_aux(nodo_insertado, nodo_actual.derecha)
+            else:
+                nodo_actual.derecha = nodo_insertado
+        
+    def agregar_nodo(self, tipo, placa, vehiculo, marca, modelo, propietario, telefono, correo, direccion, fecha):
+        nodo = Nodo(self.cantidad_citas, tipo, placa, vehiculo, marca, modelo, propietario, telefono, correo, direccion, fecha)
+        # Si el arbol esta vacio, el nodo se vuelve la raiz
+        if self.raiz == None:
+            self.raiz = nodo
+        else:
+            self.agregar_nodo_aux(nodo, self.raiz)
+        self.cantidad_citas = self.cantidad_citas + 1
