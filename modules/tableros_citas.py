0d5cd3a99c1c8ff82446e9453965dde211598021
tony1611 <antbarrantes@estudiantec.cr>
2023-06-18 23:16:45 -0600

    Fecha en ventana
    Despliegue de la fecha actual tomada del sistema en la ventana

--- modules/tableros_citas.py
+++ modules/tableros_citas.py
@@ -13,27 +13,29 @@ def tablero_citas():
         
     ventana_tablero = Toplevel()
     ventana_tablero.title("Tablero de citas")
-    ventana_tablero.maxsize(800, 225 + ((int(configuracion['cantidad_lineas']) - 5) * 35))
-    ventana_tablero.minsize(800, 225 + ((int(configuracion['cantidad_lineas']) - 5) * 35))
+    ventana_tablero.maxsize(800, 250 + ((int(configuracion['cantidad_lineas']) - 5) * 35))
+    ventana_tablero.minsize(800, 250 + ((int(configuracion['cantidad_lineas']) - 5) * 35))
     
-
+    fecha_actual = datetime.now()
+    fecha_label = Label(ventana_tablero, text = "Fecha: " + fecha_actual.strftime("%d/%m/%Y"), font = ("Arial", 15))
+    fecha_label.grid(row = 0, column = 1, pady = 3, padx = 20)
     
     linea_label = Label(ventana_tablero, text = "Linea", font = ("Arial", 15))
-    linea_label.grid(row = 0, column = 0, pady = 3, padx = 20)
+    linea_label.grid(row = 1, column = 0, pady = 3, padx = 20)
     puesto1_label = Label(ventana_tablero, text = "Puesto 1", font = ("Arial", 15))
-    puesto1_label.grid(row = 0, column = 1, pady = 3, padx = 20)
+    puesto1_label.grid(row = 1, column = 1, pady = 3, padx = 20)
     puesto2_label = Label(ventana_tablero, text = "Puesto 2", font = ("Arial", 15))
-    puesto2_label.grid(row = 0, column = 2, pady = 3, padx = 20)
+    puesto2_label.grid(row = 1, column = 2, pady = 3, padx = 20)
     puesto3_label = Label(ventana_tablero, text = "Puesto 3", font = ("Arial", 15))
-    puesto3_label.grid(row = 0, column = 3, pady = 3, padx = 20)
+    puesto3_label.grid(row = 1, column = 3, pady = 3, padx = 20)
     puesto4_label = Label(ventana_tablero, text = "Puesto 4", font = ("Arial", 15))
-    puesto4_label.grid(row = 0, column = 4, pady = 3, padx = 20)
+    puesto4_label.grid(row = 1, column = 4, pady = 3, padx = 20)
     puesto5_label = Label(ventana_tablero, text = "Puesto 5", font = ("Arial", 15))
-    puesto5_label.grid(row = 0, column = 5, pady = 3, padx = 20)
+    puesto5_label.grid(row = 1, column = 5, pady = 3, padx = 20)
     
     lineas = []
     for n in range(1, int(configuracion['cantidad_lineas']) + 1):
         linea = Label(ventana_tablero, text = str(n), font = ("Arial", 15))
-        linea.grid(row = n, column = 0, pady = 3, padx = 20)
+        linea.grid(row = n + 1, column = 0, pady = 3, padx = 20)
         lineas.append(linea)
         
